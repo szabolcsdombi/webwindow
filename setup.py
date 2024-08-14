935b7a2ab2d10e40279da46374d826a1fc3c8246
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-08-14 20:43:04 +0300

    bump to version 1.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 from setuptools import setup
 
+with open('README.md') as readme:
+    long_description = readme.read()
+
 setup(
     name='webwindow',
-    version='1.1.0',
+    version='1.2.0',
     py_modules=['webwindow'],
+    license='MIT',
+    platforms=['web'],
+    description='WebGL Window',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Szabolcs Dombi',
+    author_email='szabolcs@szabolcsdombi.com',
+    url='https://github.com/szabolcsdombi/webwindow/',
+    project_urls={
+        'Source': 'https://github.com/szabolcsdombi/webwindow/',
+        'Bug Tracker': 'https://github.com/szabolcsdombi/webwindow/issues/',
+    },
+    keywords=[
+        'WebGL',
+        'window',
+    ],
 )
